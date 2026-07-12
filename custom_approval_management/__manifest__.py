81d4ec558cabb4cc83d9ad74e4a1818e6699ee61
arqamh <arqamh@centrics.cloud>
2026-07-13 02:28:13 +0530

    Add Odoo Apps cover image

--- custom_approval_management/__manifest__.py
+++ custom_approval_management/__manifest__.py
@@ -67,6 +67,7 @@ Support: tezvosupport@gmail.com
             'custom_approval_management/static/src/js/my_pending_refresh_stack_fix.js',
         ],
     },
+    'images': ['images/main_screenshot.png'],
     'installable': True,
     'application': True,
     'price': 399.0,
